41d86ef684d943c723f88a3707f11309f3ee5f7e
rolpf <jiroxane@hotmail.fr>
2022-03-17 16:20:57 +0100

    css + new table album

--- resources/views/_songs.blade.php
+++ resources/views/_songs.blade.php
@@ -9,7 +9,7 @@
 <article class="leaderboard__profile">
       <img src="https://cdns.iconmonstr.com/wp-content/assets/preview/2014/96/iconmonstr-disc-3.png" alt="{{$s['title']}} Cover" class="leaderboard__picture">
       <span class="leaderboard__name"><span class="song-title">{{$s['title']}}</span> posted by <span class="song-artist"><a href="/users/{{ $s->user->id }}">{{ $s->user->name }}</a></span></span>
-      <span class="leaderboard__value">time</span>
+      <span class="leaderboard__value">{{$s['note']}}</span>  
     </article>
 
  
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -66,7 +66,32 @@
 @yield("content")
 </main>
 
-<footer>Copyright</footer>
+<footer>
+    <div>
+        <h3>About</h3>
+        <p>GamingOST provides you the best musics from the video games you love the most.</p>
+    </div>
+    <div class="footer-wrap">
+    <div>
+        <h3>Quick Links</h3>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/nouvelle/chanson">New Song</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </div>
+    <div>
+        <h3>Socials</h3>
+        <ul>
+            <li>Twitter</li>
+            <li>Facebook</li>
+            <li>Instagram</li>
+        </ul>
+    </div></div>
+    <div>
+    Copyright 2022
+    </div>
+</footer>
 </body>
 <script src="/js/jquery.js"></script>
 <script src="/js/divers.js"></script>
