5967cce8317df22d6761d1b692d11425e7627d26
rolpf <jiroxane@hotmail.fr>
2022-03-26 11:50:42 +0100

    fix songs

--- resources/views/_songs.blade.php
+++ resources/views/_songs.blade.php
@@ -8,34 +8,13 @@
 @foreach($songs as $s)
 <article class="leaderboard__profile">
       <img src="https://cdns.iconmonstr.com/wp-content/assets/preview/2014/96/iconmonstr-disc-3.png" alt="{{$s['title']}} Cover" class="leaderboard__picture">
-      <span class="leaderboard__name"><span class="song-title"><a href="#" class="song" data-file="{{$s->url}}">{{$s['title']}}</a></span> posted by <span class="song-artist"><a href="/users/{{ $s->user->id }}">{{ $s->user->name }}</a></span></span>
+      <span class="leaderboard__name"><span class="song-title"><a href="#" class="song" data-file="{{$s->url}}">{{$s['title']}}</a></span>
+       posted by <span class="song-artist"><a href="/users/{{ $s->user->id }}">{{ $s->user->name }}</a></span></span>
       <span class="leaderboard__value">{{$s['note']}}</span>  
     </article>
-
+@endforeach
  
 
 
-
-
-
-<!-- 
-
-    <li>
-        <a href="#" class="song" data-file="{{$s->url}}">{{$s['title']}}</a>
-         posté par <a href="/users/{{ $s->user->name }}">{{ $s->user->name }}</a> 
-        posted by <a href="/users/{{ $s->user->id }}">{{ $s->user->name }}</a>
-    </li> -->
-
-@endforeach
 <main>
-</ul>
 
-<!-- <ul>
-@foreach($songs as $s)
-    <li>
-        <a href="#" class="song" data-file="{{$s->url}}">{{$s['title']}}</a>
-        posté par <a href="/users/{{ $s->user->id }}">{{ $s->user->name }}</a>
-    </li>
-
-@endforeach
-</ul> -->
